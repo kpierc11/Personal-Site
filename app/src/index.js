359b60b823a621a1508a4c85a3cd7e588bb8a607
kpierc11 <35708070+kpierc11@users.noreply.github.com>
2021-12-30 21:05:22 -0500

    fixed index and removed the game

--- app/src/index.js
+++ app/src/index.js
@@ -3,16 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Game from "./platformer/js/Game";
 
-const game = new Game();
-
-game.initialize();
-
-if (!game) {
-    ReactDOM.render(<App />, document.getElementById("root"));
-}
-
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
