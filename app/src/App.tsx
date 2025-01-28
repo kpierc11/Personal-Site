3766cd7132ef6a64e7045a782ddd1331ccd35ef6
Emberwind-Studios <89319126+Emberwind-Studios@users.noreply.github.com>
2025-01-27 22:30:39 -0500

    removed unused import

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,7 +2,6 @@ import FadeInAnimation from "./scripts/animation/FadeInAnimation";
 import Banner from "./components/bannerSection/Banner";
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/header/Navbar";
-import PortfolioSectionTwo from "./components/portfolioSection/PortfolioSectionTwo";
 import Skills from "./components/skillsSection/Skills";
 import MobileAppSection from "./components/mobileAppSection/MobileAppSection";
 import About from "./components/aboutSection/About";
